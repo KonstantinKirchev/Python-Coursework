d52918a4d061374e01e5b5feea4c9aa3950a7318
KonstantinKirchev <konstantin1230@yahoo.com>
2020-11-13 16:20:25 -0500

    Remove unnecessary code

--- hacker_rank_challenges/arithmetic_operators.py
+++ hacker_rank_challenges/arithmetic_operators.py
@@ -1,9 +1,8 @@
-if __name__ == '__main__':
-    a = int(input().strip())
-    b = int(input().strip())
+a = int(input().strip())
+b = int(input().strip())
 if 1 <= a <= 10**10 and 1 <= b <= 10**10:
     print(a + b)
     print(a - b)
     print(a * b)
 else:
-	print("Invalid input! The numbers should be between 1 and 10000000000")
+    print("Invalid input! The numbers should be between 1 and 10000000000")
